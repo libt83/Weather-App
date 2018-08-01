227f2242f7420271bd9dc29cfde9756e6c8eb308
Brandon <deftones2001@gmail.com>
2018-07-31 23:31:15 -0500

    refactored the code to support the use of addtional callbacks

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -2,7 +2,7 @@ var exports = module.exports = {};
 
 const request = require('request');
 
-exports.geocodeAddress = (address) => {
+exports.geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
 
   request({
@@ -10,13 +10,15 @@ exports.geocodeAddress = (address) => {
       json: true
   }, (error, response, body) => {
     if(error) {
-      console.log('Unable to connect to Google servers');
+      callback('Unable to connect to Google servers');
     }else if(body.status === 'ZERO_RESULTS') {
-      console.log('Unable to find that address');
+      callback('Unable to find that address');
     }else if(body.status === 'OK') {
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
     }
   });
 };
